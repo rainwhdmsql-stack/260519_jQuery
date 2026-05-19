295659441ae7e7e9105583e9258d3639ee0cd185
rainwhdmsql-stack <rainwhdmsql@gmail.com>
2026-05-19 14:45:07 +0900

    박스 접엇다 생겻다 지웟다

---
+++ js/allevent.js
@@ -0,0 +1,34 @@
+$(function () {
+  //   slideDown
+  $(".slideDown").click(function () {
+    $(".box").slideDown(800);
+  });
+  //slideUp
+  $(".slideUp").click(function () {
+    $(".box").slideUp(1000);
+  });
+  //  slideToggle
+  $(".slideToggle").click(function () {
+    $(".box").slideToggle(800);
+  });
+  //  fadeIn
+  $(".fadeIn").click(function () {
+    $(".box").fadeIn(800);
+  });
+  //  fadeOut
+  $(".fadeOut").click(function () {
+    $(".box").fadeOut(800);
+  });
+  //  hide
+  $(".hide").click(function () {
+    $(".box").hide(800);
+  });
+  //  show
+  $(".show").click(function () {
+    $(".box").show(800);
+  });
+  //  toggle
+  $(".toggle").click(function () {
+    $(".box").toggle(800);
+  });
+});
